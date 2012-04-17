238ba433c4d09e92997bde3a114b602f5d588cd1
Helior Colorado <me@helior.info>
2012-04-17 01:27:49 -0700

    Moved all stack ui helpers/callbacks to stack-admin.inc.

--- plugins/export_ui/panels_frame_stack_ui.class.php
+++ plugins/export_ui/panels_frame_stack_ui.class.php
@@ -6,8 +6,8 @@ class panels_frame_stack_ui extends panels_frame_ui {
     $base = array(
       'access arguments' => array('access content'),
       'type' => MENU_CALLBACK,
-      'file' => 'panels_frame.admin.inc',
-      'file path' => drupal_get_path('module', 'panels_frame'),
+      'file' => 'stack-admin.inc',
+      'file path' => drupal_get_path('module', 'panels_frame') . '/includes',
       'theme callback' => 'ajax_base_page_theme',
     );
 
@@ -28,6 +28,7 @@ class panels_frame_stack_ui extends panels_frame_ui {
   }
 
   function edit_form_frames(&$form, &$form_state) {
+    ctools_include('stack-admin', 'panels_frame');
     ctools_include('plugins', 'panels');
     ctools_include('ajax');
     ctools_include('modal');
@@ -37,46 +38,7 @@ class panels_frame_stack_ui extends panels_frame_ui {
     $cache_mechanism = 'export_ui::' . $form_state['plugin']['name'];
     $cache_key = $form_state['object']->edit_cache_get_key($form_state['item'], $form_state['form type']);
 
-    $form['data'] = array(
-      '#element_validate' => array('panels_frame_stack_ui_frames_sort'),
-      '#after_build' => array('panels_frame_stack_ui_frames_after_build'),
-      '#tree' => TRUE,
-    );
-
-    foreach ($form_state['item']->data as $name => $frame) {
-      foreach (array('label', 'identifier', 'layout') as $hidden_value) {
-        $form['data'][$name][$hidden_value] = array(
-          '#type' => 'value',
-          '#value' => $frame[$hidden_value],
-        );
-      }
-
-      $layout = panels_get_layout($frame['layout']);
-      // Preview
-      $form['data'][$name]['preview'] = array(
-        '#markup' => panels_print_layout_icon($layout['name'], $layout),
-      );
-
-      // Display Title
-      $form['data'][$name]['title']['#markup'] = implode('<br />', array(
-        '<strong>' . $frame['label'] . '</strong>',
-        '<em>' . $layout['title'] . '</em>',
-      ));
-
-      // Weight
-      $form['data'][$name]['weight'] = array(
-        '#type' => 'weight',
-        '#default_value' => $frame['weight'],
-        '#attributes' => array('class' => array('panels-frame-stack-frame-weight')),
-      );
-
-      // Operations
-      $form['data'][$name]['operations'] = array(
-        '#type' => 'link',
-        '#title' => t('Configure'),
-        '#href' => 'admin/structure',
-      );
-    }
+    $form['data'] = panels_frame_stack_ui_frames_table($form_state['item']->data);
 
     $form['add'] = array(
       '#type' => 'submit',
@@ -91,39 +53,4 @@ class panels_frame_stack_ui extends panels_frame_ui {
       '#value' => url('panels_frame/ajax/stack/frame/add/' . $cache_mechanism . '/' . $cache_key, array('absolute' => TRUE)),
     );
   }
-}
-
-/**
- * After build callback for frame data table.
- */
-function panels_frame_stack_ui_frames_after_build(&$element, &$form_state) {
-  $id = 'panels-frame-stack-ui-table';
-  drupal_add_tabledrag($id, 'order', 'sibling', 'panels-frame-stack-frame-weight');
-
-  $table['header'] = array(
-    array('data' => t('Preview'), 'class' => 'previeww'),
-    array('data' => t('Title'), 'class' => 'titlee'),
-    array('data' => t('Weight'), 'class' => 'weightt'),
-    array('data' => t('Operations'), 'class' => 'operationss'),
-  );
-
-  $table['rows'] = array();
-  foreach (element_children($element) as $child) {
-    $table['rows'][] = array(
-      'class' => array('draggable'),
-      'data' => array(
-        render($element[$child]['preview']),
-        render($element[$child]['title']),
-        render($element[$child]['weight']),
-        render($element[$child]['operations'])
-      ),
-    );
-  }
-
-  $table['attributes']['id'] = $id;
-  $table['sticky'] = TRUE;
-  $table['empty'] = t('Why you no have layout?');
-
-  $element['#markup'] = theme('table', $table);
-  return $element;
 }
